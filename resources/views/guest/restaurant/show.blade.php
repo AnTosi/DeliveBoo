149b052b8b82fccc669119ab973079f87286e734
iamfrego <mate_freg@tiscali.it>
2022-02-18 00:44:39 +0100

    Restaurant info add and general structure add

--- resources/views/guest/restaurant/show.blade.php
+++ resources/views/guest/restaurant/show.blade.php
@@ -1,47 +1,60 @@
 @extends('layouts.app')
 
+@section('custom-css')
+    <link rel="stylesheet" href="{{ asset('css/restaurant_show.css') }}">
+@endsection
+
 
 @section('content')
 
-    @include('partials.wave')
-    
-    <div class="container">
-        <div class="card mb-3" style="max-width: 540px;">
-            <div class="row g-0">
-                <div class="col-md-4">
-                    <img src="{{ asset('storage/restaurant_image/' . $user->id . '/' . $user->image) }}"
-                        class="img-fluid rounded-start" alt="...">
+    <div class="container-fliud">
+        <img width="100%" height="200" style="filter: blur(20px)"
+            src="{{ asset('storage/restaurant_image/' . $user->id . '/' . $user->image) }}" alt="...">
+    </div>
+
+    <div class="container info_cart">
+        <div class="row">
+            <div class="col-9 restaurant_aside">
+                <div class="restaurant_info pt-5 pb-2 px-5 shadow-lg p-3 mb-5 bg-body ">
+                    <h1 class="fs-1 fw-bold">{{ $user->name }} <span class="fs-3"><sup>®</sup></span></h1>
+
+                    <h3>
+                        {{ $user->address }}
+                    </h3>
+
+                    @if ($user->tags)
+                        @foreach ($user->tags as $tag)
+                            @if (!$loop->last)
+                                <span class="fs-5">
+                                    {{ $tag->name }},
+                                </span>
+                            @else
+                                <span class="fs-5">
+                                    {{ $tag->name }}
+                                </span>
+                            @endif
+                        @endforeach
+                    @endif
+
+                    <p class="text-muted">Free delivery for orders of 25 € or more.</p>
                 </div>
-                <div class="col-md-8">
-                    <div class="card-body">
-                        <div class="d-flex">
-                            <img width="60" src="{{ asset('storage/restaurant_logo/' . $user->id . '/' . $user->logo) }}"
-                                alt="">
-                            <h5 class="card-title">{{ $user->name }}</h5>
-                        </div>
-                        <p class="card-text">
-                            {{ $user->address }}
-                        </p>
-                        <p class="card-text">
-                            <small class="text-muted">
-                                @if ($user->tags)
-                                    @foreach ($user->tags as $tag)
-                                        @if (!$loop->last)
-                                            <span>
-                                                {{ $tag->name }},
-                                            </span>
-                                        @else
-                                            <span>
-                                                {{ $tag->name }}
-                                            </span>
-                                        @endif
-                                    @endforeach
-                                @endif
-                            </small>
-                        </p>
+
+                <div class="row row-cols-2">
+                    <div class="col">
+                        @foreach ($user->dishes as $dish)
+                            <p>Dish</p>
+                        @endforeach
                     </div>
                 </div>
             </div>
+
+            <div class="col">
+                cart
+            </div>
+
         </div>
+
     </div>
+
+
 @endsection
