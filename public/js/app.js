65006e460a7e524517b670d13f57204be295bc1d
lucamagenes <lucamgns33@gmail.com>
2022-02-18 19:55:54 +0100

    add layout on admin blade

--- public/js/app.js
+++ public/js/app.js
@@ -53903,18 +53903,18 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Andrea\Documents\Boolean Careers\progetto_finale\DeliveBoo\resources\js\app.js */"./resources/js/app.js");
-__webpack_require__(/*! C:\Users\Andrea\Documents\Boolean Careers\progetto_finale\DeliveBoo\resources\sass\common.scss */"./resources/sass/common.scss");
-__webpack_require__(/*! C:\Users\Andrea\Documents\Boolean Careers\progetto_finale\DeliveBoo\resources\sass\app.scss */"./resources/sass/app.scss");
-__webpack_require__(/*! C:\Users\Andrea\Documents\Boolean Careers\progetto_finale\DeliveBoo\resources\sass\admin.scss */"./resources/sass/admin.scss");
-__webpack_require__(/*! C:\Users\Andrea\Documents\Boolean Careers\progetto_finale\DeliveBoo\resources\sass\home.scss */"./resources/sass/home.scss");
-__webpack_require__(/*! C:\Users\Andrea\Documents\Boolean Careers\progetto_finale\DeliveBoo\resources\sass\dashboard.scss */"./resources/sass/dashboard.scss");
-__webpack_require__(/*! C:\Users\Andrea\Documents\Boolean Careers\progetto_finale\DeliveBoo\resources\sass\dishes.scss */"./resources/sass/dishes.scss");
-__webpack_require__(/*! C:\Users\Andrea\Documents\Boolean Careers\progetto_finale\DeliveBoo\resources\sass\orders.scss */"./resources/sass/orders.scss");
-__webpack_require__(/*! C:\Users\Andrea\Documents\Boolean Careers\progetto_finale\DeliveBoo\resources\sass\create.scss */"./resources/sass/create.scss");
-__webpack_require__(/*! C:\Users\Andrea\Documents\Boolean Careers\progetto_finale\DeliveBoo\resources\sass\register.scss */"./resources/sass/register.scss");
-__webpack_require__(/*! C:\Users\Andrea\Documents\Boolean Careers\progetto_finale\DeliveBoo\resources\sass\login.scss */"./resources/sass/login.scss");
-module.exports = __webpack_require__(/*! C:\Users\Andrea\Documents\Boolean Careers\progetto_finale\DeliveBoo\resources\sass\statistics.scss */"./resources/sass/statistics.scss");
+__webpack_require__(/*! /Applications/MAMP/htdocs/laravel/DeliveBoo/resources/js/app.js */"./resources/js/app.js");
+__webpack_require__(/*! /Applications/MAMP/htdocs/laravel/DeliveBoo/resources/sass/common.scss */"./resources/sass/common.scss");
+__webpack_require__(/*! /Applications/MAMP/htdocs/laravel/DeliveBoo/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Applications/MAMP/htdocs/laravel/DeliveBoo/resources/sass/admin.scss */"./resources/sass/admin.scss");
+__webpack_require__(/*! /Applications/MAMP/htdocs/laravel/DeliveBoo/resources/sass/home.scss */"./resources/sass/home.scss");
+__webpack_require__(/*! /Applications/MAMP/htdocs/laravel/DeliveBoo/resources/sass/dashboard.scss */"./resources/sass/dashboard.scss");
+__webpack_require__(/*! /Applications/MAMP/htdocs/laravel/DeliveBoo/resources/sass/dishes.scss */"./resources/sass/dishes.scss");
+__webpack_require__(/*! /Applications/MAMP/htdocs/laravel/DeliveBoo/resources/sass/orders.scss */"./resources/sass/orders.scss");
+__webpack_require__(/*! /Applications/MAMP/htdocs/laravel/DeliveBoo/resources/sass/create.scss */"./resources/sass/create.scss");
+__webpack_require__(/*! /Applications/MAMP/htdocs/laravel/DeliveBoo/resources/sass/register.scss */"./resources/sass/register.scss");
+__webpack_require__(/*! /Applications/MAMP/htdocs/laravel/DeliveBoo/resources/sass/login.scss */"./resources/sass/login.scss");
+module.exports = __webpack_require__(/*! /Applications/MAMP/htdocs/laravel/DeliveBoo/resources/sass/statistics.scss */"./resources/sass/statistics.scss");
 
 
 /***/ })
