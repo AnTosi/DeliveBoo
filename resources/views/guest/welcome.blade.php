2b9fe6efddef00eec4956cf6166dcb8d53dfb4c8
AnTosi <andrea.tosi.uni@gmail.com>
2022-02-20 17:35:01 +0100

    reduced margin-b address

--- resources/views/guest/welcome.blade.php
+++ resources/views/guest/welcome.blade.php
@@ -29,8 +29,8 @@
         y="0px" viewBox="0 0 1431.5 113.39" style="enable-background:new 0 0 1431.5 113.39;" xml:space="preserve">
         <path class="st0" fill="#FCC045" fill-opacity="1"
             d="M-8.5,0.29l48,21.53c48,21.69,144,64.5,240,81.87s192,8.48,288-8.6c96-17.25,192-43.09,288-40.95
-                                                                                                                                                                                                                                                                                                                                                                                                     c96,2.3,192,32.19,288,43.09s192,2.02,240-2.14l48-4.32V0.29h-48c-48,0-144,0-240,0s-192,0-288,0s-192,0-288,0s-192,0-288,0
-                                                                                                                                                                                                                                                                                                                                                                                                     s-192,0-240,0H-8.5z" />
+                                                                                                                                                                                                                                                                                                                                                                                                             c96,2.3,192,32.19,288,43.09s192,2.02,240-2.14l48-4.32V0.29h-48c-48,0-144,0-240,0s-192,0-288,0s-192,0-288,0s-192,0-288,0
+                                                                                                                                                                                                                                                                                                                                                                                                             s-192,0-240,0H-8.5z" />
     </svg>
 
     <div class="container mb-5">
@@ -59,7 +59,7 @@
                                 <h5 class="card-title">
                                     <span class="capitalize fs-3 text-black"> @{{ user.name }}</span>
                                 </h5>
-                                <p class="text-decoration-none">
+                                <p class="text-decoration-none mb-1">
                                     @{{ user.address }}
                                 </p>
                                 <p class="mb-0">
