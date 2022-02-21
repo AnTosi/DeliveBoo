7f968025938fab8c2e81cc18984cfb84a20de0de
iamfrego <mate_freg@tiscali.it>
2022-02-21 10:29:29 +0100

    SVG color fixed

--- resources/views/guest/welcome.blade.php
+++ resources/views/guest/welcome.blade.php
@@ -27,7 +27,7 @@
     </div>
     <svg version="1.1" id="Livello_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
         y="0px" viewBox="0 0 1431.5 113.39" style="enable-background:new 0 0 1431.5 113.39;" xml:space="preserve">
-        <path class="st0" fill="#FCC045" fill-opacity="1"
+        <path class="st0" fill="#ffc144" fill-opacity="1"
             d="M-8.5,0.29l48,21.53c48,21.69,144,64.5,240,81.87s192,8.48,288-8.6c96-17.25,192-43.09,288-40.95c96,2.3,192,32.19,288,43.09s192,2.02,240-2.14l48-4.32V0.29h-48c-48,0-144,0-240,0s-192,0-288,0s-192,0-288,0s-192,0-288,0s-192,0-240,0H-8.5z" />
     </svg>
 
