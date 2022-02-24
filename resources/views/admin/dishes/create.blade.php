6148f411d17918712c413e67bfc8b070fd2e1eb8
Mattia311 <mattia.space98@gmail.com>
2022-02-24 17:31:56 +0100

    Add image changes

--- resources/views/admin/dishes/create.blade.php
+++ resources/views/admin/dishes/create.blade.php
@@ -1,124 +1,117 @@
 @extends('layouts.admin')
 
 @section('custom-css')
-    <link rel="stylesheet" href="{{ 'css/create.css' }}">
+<link rel="stylesheet" href="{{ 'css/create.css' }}">
 @endsection
 @section('content')
-    <div class="container lg-sm pt-3">
+<div class="container lg-sm pt-3">
 
-        {{-- back to index --}}
-        <div class="back">
-            <a class="text-muted" href="{{ route('admin.dishes.index') }}">
-                <i class="fas fa-arrow-left fa-lg fa-fw"></i>
-            </a>
+    {{-- back to index --}}
+    <div class="back">
+        <a class="text-muted" href="{{ route('admin.dishes.index') }}">
+            <i class="fas fa-arrow-left fa-lg fa-fw"></i>
+        </a>
+    </div>
+
+    <form action="{{ route('admin.dishes.store') }}" method="post" enctype="multipart/form-data">
+        <div class="d-flex justify-content-between py-3">
+            <div class="col-8">
+                <h1>Add your creation</h1>
+            </div>
+            {{-- form submit --}}
+        </div>
+        @csrf
+
+        {{-- name --}}
+        <div class="mb-3">
+            <label for="" class="form-label">Name*:</label>
+            <input type="text" name="name" id="name" class="form-control @error('name') is-invalid @enderror" placeholder="" aria-describedby="nameHelper" required value="{{ old('name') }}">
+            <small id="nameHelper" class="text-muted">Type the name of your creation</small>
+
+            @error('name')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
         </div>
 
-        <form action="{{ route('admin.dishes.store') }}" method="post" enctype="multipart/form-data">
-            <div class="d-flex justify-content-between py-3">
-                <div class="col-8">
-                    <h1>Add your creation</h1>
-                </div>
-                {{-- form submit --}}
-                <div class="col-4 text-end">
-                    <button type="submit" class="btn btn-dark">Submit</button>
-                </div>
-            </div>
-            @csrf
+        {{-- //name --}}
 
-            {{-- name --}}
-            <div class="mb-3">
-                <label for="" class="form-label">Name*:</label>
-                <input type="text" name="name" id="name" class="form-control @error('name') is-invalid @enderror"
-                    placeholder="" aria-describedby="nameHelper" required value="{{ old('name') }}">
-                <small id="nameHelper" class="text-muted">Type the name of your creation</small>
+        {{-- image --}}
 
-                @error('name')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-            </div>
+        <div class="mb-3">
+            <label for="image" class="form-label">Image*:</label>
+            <input type="file" class="form-control @error('image') is-invalid @enderror" name="image" id="image" aria-describedby="imageHelper" accept=".png, .jpg">
+            <small id="imageHelper" class="form-text text-muted">Add an image file, only .png and .jpg file are
+                accepted</small>
 
-            {{-- //name --}}
+            @error('image')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+        </div>
 
-            {{-- image --}}
+        {{-- //image --}}
 
-            <div class="mb-3">
-                <label for="image" class="form-label">Image*:</label>
-                <input type="file" class="form-control @error('image') is-invalid @enderror" name="image" id="image"
-                    aria-describedby="imageHelper" accept=".png, .jpg">
-                <small id="imageHelper" class="form-text text-muted">Add an image file, only .png and .jpg file are
-                    accepted</small>
+        {{-- ingredients --}}
 
-                @error('image')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-            </div>
+        <div class="mb-3">
+            <label for="ingredients" class="form-label">Ingredients*:</label>
+            <textarea type="textarea" rows="3" class="form-control @error('ingredients') is-invalid @enderror" name="ingredients" id="ingredients" aria-describedby="ingredientsHelper" placeholder="Ingredients" required>{{ old('ingredients') }}</textarea>
+            <small id="ingredientsHelper" class="form-text text-muted">Write your dish' ingredients</small>
 
-            {{-- //image --}}
+            @error('ingredients')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+        </div>
 
-            {{-- ingredients --}}
+        {{-- //ingredients --}}
 
-            <div class="mb-3">
-                <label for="ingredients" class="form-label">Ingredients*:</label>
-                <textarea type="textarea" rows="3" class="form-control @error('ingredients') is-invalid @enderror"
-                    name="ingredients" id="ingredients" aria-describedby="ingredientsHelper" placeholder="Ingredients"
-                    required>{{ old('ingredients') }}</textarea>
-                <small id="ingredientsHelper" class="form-text text-muted">Write your dish' ingredients</small>
+        {{-- description --}}
 
-                @error('ingredients')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-            </div>
+        <div class="mb-3">
+            <label for="description" class="form-label">Description*:</label>
+            <textarea type="textarea" rows="5" class="form-control @error('description') is-invalid @enderror" name="description" id="description" aria-describedby="descriptionHelper" placeholder="Description" required>{{ old('description') }}</textarea>
+            <small id="descriptionHelper" class="form-text text-muted">Describe your dish</small>
 
-            {{-- //ingredients --}}
+            @error('description')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+        </div>
 
-            {{-- description --}}
+        {{-- //description --}}
 
-            <div class="mb-3">
-                <label for="description" class="form-label">Description*:</label>
-                <textarea type="textarea" rows="5" class="form-control @error('description') is-invalid @enderror"
-                    name="description" id="description" aria-describedby="descriptionHelper" placeholder="Description"
-                    required>{{ old('description') }}</textarea>
-                <small id="descriptionHelper" class="form-text text-muted">Describe your dish</small>
+        {{-- price --}}
 
-                @error('description')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-            </div>
+        <div class="mb-3">
+            <label for="price" class="form-label">Price in €*:</label>
+            <input type="number" class="form-control" name="price" id="price" aria-describedby="priceHelper" step="0.01" value="{{ old('price') }}" required placeholder="€">
+            <small id="priceHelper" class="form-text text-muted">Write your dish' price in €</small>
 
-            {{-- //description --}}
+            @error('price')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
+        </div>
 
-            {{-- price --}}
+        {{-- //price --}}
 
-            <div class="mb-3">
-                <label for="price" class="form-label">Price in €*:</label>
-                <input type="number" class="form-control" name="price" id="price" aria-describedby="priceHelper"
-                    step="0.01" value="{{ old('price') }}" required placeholder="€">
-                <small id="priceHelper" class="form-text text-muted">Write your dish' price in €</small>
+        {{-- avilability --}}
 
-                @error('price')
-                    <div class="alert alert-danger">{{ $message }}</div>
-                @enderror
-            </div>
+        <div class="form-check">
+            <input type="hidden" class="form-check-input" name="visibility" id="visibility" value="0">
+            <input type="checkbox" class="form-check-input" name="visibility" id="visibility" value="1">
+            <label name="visibility" class="form-check-label" for="visibility">
+                Dish availability
+            </label>
+        </div>
 
-            {{-- //price --}}
-
-            {{-- avilability --}}
-
-            <div class="form-check">
-                <input type="hidden" class="form-check-input" name="visibility" id="visibility" value="0">
-                <input type="checkbox" class="form-check-input" name="visibility" id="visibility" value="1">
-                <label name="visibility" class="form-check-label" for="visibility">
-                    Dish availability
-                </label>
-            </div>
-
-            {{-- //avilability --}}
+        {{-- //avilability --}}
 
 
-            <div class="pt-3 pb-2 text-muted">
-                <p>All the fields with * are required</p>
-            </div>
+        <div class="pt-3 pb-2 text-muted">
+            <p>All the fields with * are required</p>
+        </div>
+        <div class="col-4 submit text-end">
+            <button type="submit" class="btn btn-dark">Submit</button>
+        </div>
 
-        </form>
-    </div>
+    </form>
+</div>
 @endsection
--- resources/views/guest/restaurant/order.blade.php
+++ resources/views/guest/restaurant/order.blade.php
@@ -1,86 +1,78 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="div">
-        <div class="container py-5">
-            <h1 class="text-center">
-                Checkout:
-            </h1>
-            <div class="row py-5 row-cols-3 justify-content-evenly g-3">
-                @foreach ($listaOrdine as $id => $qty)
-                    @foreach ($piatti as $piatto)
-                        @if ($id == $piatto->id)
-                            <div class="col d-flex justify-content-center">
-                                <div class="card border-0 text-start shadow-lg"
-                                    style="width: 268px; height:100%; padding: 0;border-radius: 1rem;">
-                                    <img style="width: 100%;height: 100%;object-fit: cover;border-radius: 1rem;"
-                                        class="card-img-top" src="{{ asset('Storage/' . $piatto->image) }}" alt="#">
-                                    <div class="card-body">
-                                        <h4 class="card-title mb-3">{{ $piatto->name }}</h4>
-                                        <p class="card-text">Quantity: {{ $qty }}</p>
-                                        <p class="card-text">Total dish price: {{ $piatto->price * $qty }} € </p>
-                                    </div>
-                                </div>
-                            </div>
-                        @endif
-                    @endforeach
-                @endforeach
+<div class="div">
+    <div class="container py-5">
+        <h1 class="text-center">
+            Checkout:
+        </h1>
+        <div class="row py-5 row-cols-3 justify-content-evenly g-3">
+            @foreach ($listaOrdine as $id => $qty)
+            @foreach ($piatti as $piatto)
+            @if ($id == $piatto->id)
+            <div class="col d-flex justify-content-center">
+                <div class="card border-0 text-start shadow-lg" style="width: 268px; height:100%; padding: 0;border-radius: 1rem;">
+                    @if ($piatto->image == null)
+                    <img style="width: 100%;height: 100%;object-fit: cover;border-radius: 1rem;" class="card-img-top" src="{{ asset('img/no-food-image.jpeg') }}" alt="">
+                    @else
+                    <img style="width: 100%;height: 100%;object-fit: cover;border-radius: 1rem;" class="card-img-top" src="{{ asset('Storage/' . $piatto->image) }}" alt="">
+                    @endif
+                    <div class="card-body">
+                        <h4 class="card-title mb-3">{{ $piatto->name }}</h4>
+                        <p class="card-text">Quantity: {{ $qty }}</p>
+                        <p class="card-text">Total dish price: {{ $piatto->price * $qty }} € </p>
+                    </div>
+                </div>
             </div>
-            <div class="mb-3 d-flex flex-row justify-content-center">
-                <form method="post" action="{{ route('payment.pay') }}">
-                    @csrf
-                    <div class="form-group">
-                        <label for="customer_name" class="form-label fs-4">Write your name</label>
-                        <input type="text" name="customer_name" id="customer_name"
-                            class="form-control bg-white border-0 shadow-lg " placeholder="Write your name"
-                            aria-describedby="helpName">
-                        <small id="helpName" class="text-muted">Name</small>
-                    </div>
-                    <input type="number" disabled value="{{$restaurant->id}}" name="user_id">
-                    <div class="form-group">
-                        <label for="email"  class="form-label fs-4">Write your e-mail</label>
-                        <input type="email" required name="email" id="email"
-                            class="form-control bg-white border-0 shadow-lg " placeholder="Write your name"
-                            aria-describedby="helpName">
-                        <small id="helpName" class="text-muted">E-Mail</small>
-                    </div>
-                    <div class="form-group">
-                        <label for="address" class="form-label fs-3">Write your Address</label>
-                        <input type="text" name="address" id="address" class="form-control bg-white border-0 shadow-lg"
-                            placeholder="Address" aria-describedby="helpAddress">
-                        <small id="helpAddress" class="text-muted">Address</small>
-                    </div>
+            @endif
+            @endforeach
+            @endforeach
+        </div>
+        <div class="mb-3 d-flex flex-row justify-content-center">
+            <form method="post" action="{{ route('payment.pay') }}">
+                @csrf
+                <div class="form-group">
+                    <label for="customer_name" class="form-label fs-4">Write your name</label>
+                    <input type="text" name="customer_name" id="customer_name" class="form-control bg-white border-0 shadow-lg " placeholder="Write your name" aria-describedby="helpName">
+                    <small id="helpName" class="text-muted">Name</small>
+                </div>
+                <input type="number" disabled value="{{$restaurant->id}}" name="user_id">
+                <div class="form-group">
+                    <label for="email" class="form-label fs-4">Write your e-mail</label>
+                    <input type="email" required name="email" id="email" class="form-control bg-white border-0 shadow-lg " placeholder="Write your name" aria-describedby="helpName">
+                    <small id="helpName" class="text-muted">E-Mail</small>
+                </div>
+                <div class="form-group">
+                    <label for="address" class="form-label fs-3">Write your Address</label>
+                    <input type="text" name="address" id="address" class="form-control bg-white border-0 shadow-lg" placeholder="Address" aria-describedby="helpAddress">
+                    <small id="helpAddress" class="text-muted">Address</small>
+                </div>
 
-                    <div class="form-group d-flex">
-                        <div class="dish_price_only me-2">
-                            <label for="dish_price" class="form-label fs-3">Dish Price</label>
-                            <input class="form-control" name="dish_price" id="dish_price" type="text"
-                                value="{{ $prezzo_totale }}" readonly>
-                        </div>
-                        <div class="dish_price_delivery ms-2">
-                            <label for="dish_price" class="form-label fs-3">Total Price</label>
+                <div class="form-group d-flex">
+                    <div class="dish_price_only me-2">
+                        <label for="dish_price" class="form-label fs-3">Dish Price</label>
+                        <input class="form-control" name="dish_price" id="dish_price" type="text" value="{{ $prezzo_totale }}" readonly>
+                    </div>
+                    <div class="dish_price_delivery ms-2">
+                        <label for="dish_price" class="form-label fs-3">Total Price</label>
 
-                            @if ($prezzo_totale < 25)
-                                <input class="form-control" type="text" name="total_price" id="total_price"
-                                    value="{{ $prezzo_totale + 2.5 }}" readonly>
-                                <small class="text-danger">Pay 2.50€ for orders under 25€</small>
+                        @if ($prezzo_totale < 25) <input class="form-control" type="text" name="total_price" id="total_price" value="{{ $prezzo_totale + 2.5 }}" readonly>
+                            <small class="text-danger">Pay 2.50€ for orders under 25€</small>
                             @else
-                                <input class="form-control" type="text" name="total_price" id="total_price"
-                                    value="{{ $prezzo_totale }}" readonly>
-                                <small class="text-success">Free Shipping</small>
+                            <input class="form-control" type="text" name="total_price" id="total_price" value="{{ $prezzo_totale }}" readonly>
+                            <small class="text-success">Free Shipping</small>
                             @endif
-                        </div>
                     </div>
+                </div>
 
-                    <div class="d-flex justify-content-center mt-3">
-                        <button class="btn bg_secondary_smooth text_secondary fw-bold " type="submit"
-                            href="{{ route('payment.pay') }}">
-                            Proceed to Payment
-                        </button>
-                    </div>
+                <div class="d-flex justify-content-center mt-3">
+                    <button class="btn bg_secondary_smooth text_secondary fw-bold " type="submit" href="{{ route('payment.pay') }}">
+                        Proceed to Payment
+                    </button>
+                </div>
 
-                </form>
-            </div>
+            </form>
         </div>
     </div>
+</div>
 @endsection
