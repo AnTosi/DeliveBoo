9dd83c113e62827efd0d6a5ee1954f91f576189d
Vale2197 <valentinatrebbi97@gmail.com>
2022-02-24 09:59:05 +0100

    fix filter

--- resources/js/app.js
+++ resources/js/app.js
@@ -88,7 +88,7 @@ const app = new Vue({
           this.filteredUsers = r.data
         })
       } else {
-        this.filteredUsers = this.users
+        this.filteredUsers = 0
       }
 
       // this.callApi()
@@ -172,7 +172,7 @@ const app = new Vue({
         })
 
       } else {
-        this.filteredRest = []
+        this.filteredRest = 0
       }
 
     },
