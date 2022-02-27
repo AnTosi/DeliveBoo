533e9a78616b087f39aec2451564a33e10044523
AnTosi <andrea.tosi.uni@gmail.com>
2022-02-27 12:14:23 +0100

    missed the save on last commit

--- app/Http/Controllers/Admin/StatisticController.php
+++ app/Http/Controllers/Admin/StatisticController.php
@@ -3,14 +3,24 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Models\Order;
 use Illuminate\Http\Request;
+use App\Models\Order;
+use App\User;
 
 class StatisticController extends Controller
 {
     //
     public function index()
     {
+
+        // $user = User::find(Auth::id());
+        // $orders = $user->orders()->orderByDesc('id')->paginate(12);
+
+        // foreach ($orders as $index => $value) {
+        //     # code...
+        // }
+
+
 
         return view('admin.statistics');
     }
